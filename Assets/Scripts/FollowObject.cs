f86d3bd3a71e6d217b3e1c635d40353701e3b0b0
Dmitri Kolytchev <kolytchev.d@gmail.com>
2016-08-20 17:58:41 -0400

    adding main menu background and single-player camera

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+	public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null) {
+			transform.position = target.transform.position;
+		}
+	}
+}
