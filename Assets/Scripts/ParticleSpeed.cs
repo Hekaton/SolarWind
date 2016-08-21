f1e4513ed953087a245f642ad5e23400109da8a2
Catalin <jeremiecoul@hotmail.com>
2016-08-20 21:07:58 -0400

    Bob

---
+++ Assets/Scripts/ParticleSpeed.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleSpeed : MonoBehaviour {
+
+    public ParticleSystem pSystem;
+
+    public Rigidbody shipVelocity;
+    
+
+	// Use this for initialization
+	void Start () {
+        pSystem = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (shipVelocity.velocity == Vector3.zero && !pSystem.isPaused)
+        {
+            pSystem.Pause();
+        }
+        else if(shipVelocity.velocity != Vector3.zero  && pSystem.isPaused)
+        {
+            pSystem.Play();
+        }
+
+	}
+}
